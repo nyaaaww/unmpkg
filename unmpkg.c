9da9f828e3fe2cbe4f38f5c24d69521002621585
nyaaaww <siyueba73@gmail.com>
2024-08-24 00:50:14 +0800

    fine this program.

--- unmpkg.c
+++ unmpkg.c
@@ -1,107 +1,163 @@
+/*
+ * Copyright (C) 2024 nyaaaww
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+char fnamel[1024][1024];
 
 long get_file_size(const char *filename) {
-    FILE *file = fopen(filename, "rb");
-    if (file == NULL) {
-        perror("Error opening file for size check");
-        return -1;
-    }
-    long size;
-    fseek(file, 0, SEEK_END); 
-    size = ftell(file); 
-    rewind(file);     
-    fclose(file);    
-    return size;
+  FILE *file = fopen(filename, "rb");
+  if (file == NULL) {
+    perror("Error opening file for size check");
+    return -1;
+  }
+  long size;
+  fseek(file, 0, SEEK_END);
+  size = ftell(file);
+  rewind(file);
+  fclose(file);
+  return size;
 }
 
-int print_head(const char *file_name){
-    FILE *file;
-    int data;
-    size_t result;
+int create_dir_recursive(const char *path) {
+  char *cpy_path = strdup(path);
+  if (!cpy_path) {
+    perror("strdup failed");
+    return -1;
+  }
 
-    // 打开文件
-    file = fopen(file_name, "rb"); 
-    if (file == NULL) {
-        perror("Error opening file");
+  char *cur_path = cpy_path;
+  while (*cur_path) {
+    if (*cur_path == '/') {
+      *cur_path = '\0';  // temp cut dir to create
+      if (mkdir(cpy_path, 0777) == -1 &&
+          errno != EEXIST) {  // check dir is exists.
+        free(cpy_path);
         return -1;
+      }
+      *cur_path = '/';
     }
+    cur_path++;
+  }
 
-    result = fread(&data, 4, 1, file);
-    if (result != 1) {
-        perror("Error reading file");
-        fclose(file);
-        return -1;
-    }
-    printf("Version Long: %d\n", data);
-    char ver[data+1];
-    memset(ver,0x00,sizeof(ver));
-    fread(ver, 8, 1, file);
-    printf("Version : %s\n", ver);
+  free(cpy_path);
+  return 0;
+}
 
-    int nof;
-    fread(&nof, 4, 1, file);
-    printf("Num of file : %d\n", nof);
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+    fprintf(stderr, "A tool decode the wallpaper engine's mpkg file.\n");
+    fprintf(stderr, "Example: %s test.mpkg\n", argv[0]);
+    return EXIT_FAILURE;
+  }
 
-    for(int i=0;i<nof;i++){
+  FILE *file;
+  int data;
+  size_t result;
+  memset(fnamel, 0x00, sizeof(fnamel));
 
+  file = fopen(argv[1], "rb");
+  if (file == NULL) {
+    perror("Error opening file");
+    return -1;
+  }
+
+  result = fread(&data, 4, 1, file);
+  if (result != 1) {
+    perror("Error reading file");
+    fclose(file);
+    return -1;
+  }
+  printf("Version Long: %d\n", data);
+  char ver[data + 1];
+  memset(ver, 0x00, sizeof(ver));
+  fread(ver, 8, 1, file);
+  printf("Version : %s\n", ver);
+
+  int nof;
+  fread(&nof, 4, 1, file);
+  printf("Num of file : %d\n", nof);
+
+  int *file_long = malloc(nof * sizeof(int));
+  for (int i = 0; i < nof; i++) {
     int name_long;
     fread(&name_long, 4, 1, file);
     printf("\nName long : %d\n", name_long);
 
-
-    char fnamel[nof][name_long+1];
-    memset(fnamel,0x00,sizeof(fnamel));
+    // char fnamel[nof][name_long + 1];
+    //   memset(fnamel, 0x00, sizeof(fnamel));
     fread(fnamel[i], name_long, 1, file);
     printf("Name : %s\n", fnamel[i]);
 
-    // 将文件指针下移4个字节
+    // move file ptr down 4 bytes
     if (fseek(file, 4, SEEK_CUR) != 0) {
-        perror("Error seeking in file");
-        fclose(file);
-        return -1;
+      perror("Error seeking in file");
+      fclose(file);
+      return -1;
     }
 
-    int file_long;
-    fread(&file_long, 4, 1, file);
-    printf("File long : %d\n", file_long);
-    }
-    fclose(file);
-    return 0;
-}
+    fread(file_long + i, sizeof(int), 1, file);
+    printf("File long : %d\n\n", file_long[i]);
+    /*   for (int a = 0; a < nof; a++) {
+         printf("File long : %d\n", file_long[a]);
+       }*/
 
-int main(int argc,char *argv[]){
+    if (i == nof - 1) {
+      //   printf("DEBUG INFO: into write\n");
+      int s;
+      for (int l = 0; l < nof; l++) {
+        //  printf("DEBUG PATH:%s\nDEBUG INFO: L is %d\n", fnamel[l], l);
+        if (fnamel[l] == NULL) {
+          continue;
+        }
+        char *buf;
+        buf = (char *)malloc(file_long[l] + 1);
+        fread(buf, file_long[l], 1, file);
+        s = create_dir_recursive(fnamel[l]);
+        if (s == -1) {
+          perror("create dir failed");
+          return -1;
+        }
+        FILE *wfile;
+        wfile = fopen(fnamel[l], "wb");
+        if (wfile == NULL) {
+          perror("Error opening file");
+          return EXIT_FAILURE;
+        }
 
-    FILE *file;
-    int data;
-    char *buf;
-    long file_size=get_file_size(argv[1]);
-    size_t result;
-
-    buf=(char *)malloc(file_size+1);
-    if (buf == NULL) {
-        perror("Error allocating memory");
-        fclose(file);
-        return -1;
-    }
-
-print_head(argv[1]);
-
-    file = fopen(argv[1], "rb");
-    if (file == NULL) {
-        perror("Error opening file");
-        return -1;
-    }
-
-    result = fread(buf, 1, file_size, file);
-    if (result != file_size) {
-        perror("Error reading file");
+        size_t bytes_written;
+        bytes_written = fwrite(buf, file_long[l], 1, wfile);
+        if (bytes_written != 1) {
+          perror("Error writing to file");
+          fclose(wfile);
+          return EXIT_FAILURE;
+        }
         free(buf);
-        fclose(file);
-        return -1;
+        fclose(wfile);
+      }
     }
-    // 关闭文件
-    fclose(file);
-
-    return 0;
+  }
+  free(file_long);
+  fclose(file);
+  return 0;
 }
